a7985b61b424b242f6e2274727be6f6f0e87d346
nsnolen <nolensara1@gmail.com>
2021-03-18 12:06:03 -0600

    Initial push

---
+++ lib/workout.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "workout/version"
+
+module Workout
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/workout/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Workout
+  VERSION = "0.1.0"
+end
