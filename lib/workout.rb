cf522966b916ef6687fe7057afde013e8c750cb8
nsnolen <nolensara1@gmail.com>
2021-03-18 17:19:05 -0600

    updated require_relative

--- lib/workout.rb
+++ lib/workout.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "./workout/version"
-require_relative "workout/scraper"
+#require_relative "./workout/scraper"
 require_relative "./workout/cli"
 
 module Workout
--- lib/workout/cli.rb
+++ lib/workout/cli.rb
@@ -1,15 +1,12 @@
-class Workout::cli 
+class Workout::CLI 
 
     def call 
         puts "Welcome to Fight Camp workouts!"
         workout_type
-        
-
-
     end
 
     def workout_type
-        puts %w{How would you like to filter your kickboxing workouts?"
+        puts %W{How would you like to filter your kickboxing workouts?"
         1. Coach
         2. Rounds
         3. Time}
