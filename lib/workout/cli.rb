8f992ddb028bab5d260922ad552e4c32bf593173
nsnolen <nolensara1@gmail.com>
2021-03-19 08:59:51 -0600

    iteration through coach, rounds, and time options

--- lib/workout/cli.rb
+++ lib/workout/cli.rb
@@ -5,6 +5,9 @@ class Workout::CLI
         puts "Welcome to Fight Camp workouts!"
         workout_type
         input_workout_type
+        display_chosen_input
+       # input_learn_more
+       # display_learn_more
     end
 
     def workout_type
@@ -25,11 +28,21 @@ class Workout::CLI
 
     def display_chosen_input(input)
         if input == 1 
-            puts "flo, smash" 
+           # list_coaches 
+            @coaches = ["Flo", "Smash", "Smash"]
+            @coaches.uniq.each.with_index(1) do |coach, index| 
+                puts "#{index}. #{coach}"
+            end
         elsif input == 2
-            puts "1, 2, 3"
+           @rounds = ["4", "6", "8", "10"]
+           @rounds.uniq.each.with_index(1) do |rounds, index| 
+            puts "#{index}. #{rounds}"
+        end
         elsif input == 3
-            puts "12, 16, 30"
+            @time = ["12", "16", "20", "30"]
+            @time.uniq.each.with_index(1) do |time, index| 
+                puts "#{index}. #{time} minutes"
+            end
         end
         
     end
