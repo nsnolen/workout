003713a0ec6ce37d2aecd682a9b924d17811ef9f
nsnolen <nolensara1@gmail.com>
2021-03-19 11:10:26 -0600

    updated display_chosen_input

--- lib/workout/cli.rb
+++ lib/workout/cli.rb
@@ -4,8 +4,10 @@ class Workout::CLI
     def call 
         puts "Welcome to Fight Camp workouts!"
         workout_type
-        input_workout_type
-        display_chosen_input
+        gets_workout_sort_by_preference
+        gets_workout_list
+    
+        #input_next_selection
        # input_learn_more
        # display_learn_more
     end
@@ -17,35 +19,82 @@ class Workout::CLI
         3. Time}
     end
 
-    def input_workout_type
-        input = gets.strip.to_i
-        display_chosen_input(input) if valid(input)
+    def gets_workout_sort_by_preference
+        view_by_input = gets.strip.to_i
+       display_chosen_input(view_by_input) if view_by_input.to_i <= 3 && view_by_input.to_i > 0
+    end
+    
+
+    def display_chosen_input(view_by_input)
+
+        if view_by_input == 1 
+            display_coaches
+               
+        elsif view_by_input == 2
+            display_rounds
+          
+        elsif view_by_input == 3
+            display_time
+           
+        end
     end
 
-    def valid(input)
-        input.to_i <= 3 && input.to_i > 0
+    def display_coaches 
+        puts "Which coach?! Select the coach you'd like to workout with, by typing the corresponding number!"
+        @coaches = ["Flo", "Smash", "Smash"]
+        @coaches.uniq.each.with_index(1) do |coach, index| 
+            puts "#{index}. #{coach}"
+        end
     end
 
-    def display_chosen_input(input)
-        if input == 1 
-           # list_coaches 
-            @coaches = ["Flo", "Smash", "Smash"]
-            @coaches.uniq.each.with_index(1) do |coach, index| 
-                puts "#{index}. #{coach}"
-            end
-        elsif input == 2
-           @rounds = ["4", "6", "8", "10"]
-           @rounds.uniq.each.with_index(1) do |rounds, index| 
-            puts "#{index}. #{rounds}"
+    def display_rounds 
+        puts "How many round?! Select the number of the rounds you'd like to workout by typing the corresponding number!"
+        @rounds = ["4", "6", "8", "10"]
+        @rounds.uniq.each.with_index(1) do |rounds, index| 
+         puts "#{index}. #{rounds}"
+     end
+    end 
+
+    def display_time 
+        puts "How long?! Select the time you'd like to workout by typing the corresponding number!"
+        @time = ["12", "16", "20", "30"]
+        @time.uniq.each.with_index(1) do |time, index| 
+            puts "#{index}. #{time} minutes"
         end
-        elsif input == 3
-            @time = ["12", "16", "20", "30"]
-            @time.uniq.each.with_index(1) do |time, index| 
-                puts "#{index}. #{time} minutes"
-            end
-        end
-        
     end
+
+    def valid(input, data)
+        input.to_i <= data.length && input.to_i > 0
+    end
+
+
+    def gets_workout_list
+        selected_workout_sort = gets.strip.to_i 
+        display_workout_list(selected_workout_sort)
+    end
+
+    def display_workout_list(selected_workout_sort)
+        if view_by_input == 1
+            display_workouts_by_coaches
+        elsif view_by_input == 2 
+            display_workouts_by_rounds 
+        elsif view_by_input == 3 
+            display_workouts_by_time 
+        end
+            
+
+    end
+
+    def display_workouts_by_coaches(selected_workout_sort)
+        input_two = selected_workout_sort.to_i - 1
+        workouts = ["workout 1", "workout 2", "workout 3"]
+        workout.each.with_index do |wo, i|
+            puts "#{i}. #{wo}"
+        end
+    end
+
+
+   
 
 end
 
